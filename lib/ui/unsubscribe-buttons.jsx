be99079477be537784f15971925e3d720b4c2353
Kyle King <kmking72@gmail.com>
2016-06-05 23:03:11 -0400

    Fixed icon resolution (ceil() and not floor() )

--- lib/ui/unsubscribe-buttons.jsx
+++ lib/ui/unsubscribe-buttons.jsx
@@ -109,9 +109,9 @@ class ThreadUnsubscribeToolbarButton extends ThreadUnsubscribeButton {
     if (typeof scale === 'undefined') {
       // Add error checking to make sure an icon is available:
       // PREVIOUSLY: scale = window.devicePixelRatio || 1;
-      scale = Math.floor(window.devicePixelRatio);
-      if (scale !== 1 || scale !== 2) { scale = 1; }
-      // console.log(`Calculated scale: ${scale}`);
+      scale = Math.ceil(window.devicePixelRatio);
+      console.log(`Calculated scale: ${scale}`);
+      if (scale !== 1 || scale !== 2) { scale = 2; }
     }
 
     url += name;
